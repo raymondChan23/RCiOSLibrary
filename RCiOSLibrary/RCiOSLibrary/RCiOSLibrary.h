1a7e56b04beb36d1b4231efb894f3ff908662bdf
Raymond_Chan <raymondchan@jetco.com.hk>
2018-10-09 14:34:55 +0800

    add pod

---
+++ RCiOSLibrary/RCiOSLibrary/RCiOSLibrary.h
@@ -0,0 +1,19 @@
+//
+//  RCiOSLibrary.h
+//  RCiOSLibrary
+//
+//  Created by Raymond_Chan on 9/10/2018.
+//  Copyright © 2018 Raymond_Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RCiOSLibrary.
+FOUNDATION_EXPORT double RCiOSLibraryVersionNumber;
+
+//! Project version string for RCiOSLibrary.
+FOUNDATION_EXPORT const unsigned char RCiOSLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RCiOSLibrary/PublicHeader.h>
+
+
---
+++ RCiOSLibrary/RCiOSLibraryTests/RCiOSLibraryTests.swift
@@ -0,0 +1,34 @@
+//
+//  RCiOSLibraryTests.swift
+//  RCiOSLibraryTests
+//
+//  Created by Raymond_Chan on 9/10/2018.
+//  Copyright © 2018 Raymond_Chan. All rights reserved.
+//
+
+import XCTest
+@testable import RCiOSLibrary
+
+class RCiOSLibraryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
